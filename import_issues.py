874c99ebb08ecb6c3d8eb19b17ba3cc92cfaac02
yuvaanshis <60852651+yuvaanshis@users.noreply.github.com>
2022-05-06 14:48:25 +0530

    Update import_issues.py

--- import_issues.py
+++ import_issues.py
@@ -8,26 +8,17 @@ from json_helpers import DateTimeEncoder
 
 ORG = 'philips-software'
 REPO = 'philips-software.github.io'
-# GITHUB_TOKEN = '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'
+GITHUB_TOKEN = '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'
 FILENAME_ISSUES = ORG + 'issues.json'
 
-
-from github import Github
-
-g = Github(base_url="https://github.com/api/v3", login_or_token="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")
-r = g.get_repo("philips-software/philips-software.github.io")
-for issue in r.get_issues(state='open'):
-    for comment in issue.get_comments():
-        print(comment)
-data = {}
 
 if os.path.isfile(FILENAME_ISSUES):
 	f = open(FILENAME_ISSUES)
 	data = json.load(f)
 	f.close()
 
-#gh = Github(GITHUB_TOKEN)
-gh = Github(g)
+gh = Github(GITHUB_TOKEN)
+#gh = Github()
 
 if REPO not in data.keys():
 	data[REPO] = {}
