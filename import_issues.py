a4179584a455c71e1019b5b74aacd1d994ecfce9
yuvaanshis <60852651+yuvaanshis@users.noreply.github.com>
2022-05-06 15:41:12 +0530

    Update import_issues.py

--- import_issues.py
+++ import_issues.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python
-#import requests
-import github3, json, os.path
+
+import github3
 import pandas as pd
 
-from json_helpers import DateTimeEncoder
-
+GITHUB_TOKEN = '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'
 ORG = 'yuvaanshis'
 REPO = 'new_batch'
-GITHUB_TOKEN = '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'
+
 FILENAME_ISSUES = ORG + 'issues.json'
 
-#gh = Github(GITHUB_TOKEN)
-#gh = Github()
-#gh = github3.login(token=GITHUB_TOKEN)
-#gh = Github("GITHUB_TOKEN")
 gh = github3.login(token=GITHUB_TOKEN)
 
 issues = []
 for i, issue in enumerate(gh.issues_on(ORG, REPO, state='all')):
     issues.append({'closed': issue.is_closed(), 'created': issue.created_at})
     print(f'got {i} issues...')
-	
+
 df = pd.DataFrame(issues)
 with open(FILENAME_ISSUES, 'w') as f:
     f.write(df.to_json())
